ec3bc0f3868fb4df58057fab96d901d8b046c819
Yakin <sureonly88@gmail.com>
2024-05-29 22:33:15 +0800

    pedami_inven

--- app/Filament/Widgets/KontrakWidget.php
+++ app/Filament/Widgets/KontrakWidget.php
@@ -5,7 +5,7 @@ namespace App\Filament\Widgets;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
-use App\Models\data_r2r4;
+use App\Models\Kontrak;
 use DB;
 
 class KontrakWidget extends BaseWidget
@@ -15,12 +15,12 @@ class KontrakWidget extends BaseWidget
         return $table
             ->query(
                 //data_r2r4::query()->where("kode_brg", 'KR001')
-                data_r2r4::query()->where(DB::raw("cast(now() as date)"), ">", DB::raw("DATE_SUB(pajak, INTERVAL 3 MONTH)"))
+                Kontrak::query()->where(DB::raw("cast(now() as date)"), ">", DB::raw("DATE_SUB(tgl_akhir, INTERVAL 3 MONTH)"))
             )
             ->columns([
-                Tables\Columns\TextColumn::make('kode_brg'),
-                Tables\Columns\TextColumn::make('nm_brg'),
-                Tables\Columns\TextColumn::make('pajak'),
+                Tables\Columns\TextColumn::make('no_kontrak'),
+                Tables\Columns\TextColumn::make('judul'),
+                Tables\Columns\TextColumn::make('tgl_akhir'),
             ])
             ->defaultPaginationPageOption(5);
     }
