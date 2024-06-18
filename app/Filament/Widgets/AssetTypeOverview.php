a3bd74384bbd7613cba18dc6294486f2234c8159
Yakin <sureonly88@gmail.com>
2024-06-18 10:48:21 +0800

    penambahan code

--- app/Filament/Widgets/AssetTypeOverview.php
+++ app/Filament/Widgets/AssetTypeOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\data_r2r4;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Asset;
@@ -13,7 +14,10 @@ class AssetTypeOverview extends BaseWidget
         return [
             Stat::make('Peralatan Komputer', Asset::query()->where('kelompok_asset', 'komputer')->count()),
             Stat::make('Perabotan Kantor', Asset::query()->where('kelompok_asset', 'kantor')->count()),
-            Stat::make('Kendaraan', Asset::query()->where('kelompok_asset', 'kendaraan')->count()),
+            Stat::make('Kendaraan R2 Operasional', data_r2r4::query()->where('jns_brg', 'R2 Operasional')->count()),
+            Stat::make('Kendaraan R2 Dinas', data_r2r4::query()->where('jns_brg', 'R2 Dinas')->count()),
+            Stat::make('Kendaraan R4 Operasional', data_r2r4::query()->where('jns_brg', 'R4 Operasional')->count()),
+            Stat::make('Kendaraan R4 Dinas', data_r2r4::query()->where('jns_brg', 'R4 Dinas')->count()),
         ];
     }
 }
