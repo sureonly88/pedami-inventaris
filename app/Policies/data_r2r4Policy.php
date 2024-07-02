6610642c2d569451fafa4baa15a6fb7ffc9340d4
Yakin <sureonly88@gmail.com>
2024-07-02 10:20:23 +0800

    tambah_policy

---
+++ app/Policies/data_r2r4Policy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\data_r2r4;
+
+class data_r2r4Policy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function update(User $user): bool
+    {
+        return $user->role === "admin";
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->role === "admin";
+    }
+
+    public function delete(User $user): bool
+    {
+        return $user->role === "admin";
+    }
+
+    public function deleteAny(User $user): bool
+    {
+        return $user->role === "admin";
+    }
+}
