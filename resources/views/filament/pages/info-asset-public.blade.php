e8dbd347d1fd11087baba5f813d5788be983f4cc
Yakin <sureonly88@gmail.com>
2025-03-02 21:56:36 +0800

    tambah barcode

--- resources/views/filament/pages/info-asset-public.blade.php
+++ resources/views/filament/pages/info-asset-public.blade.php
@@ -1,15 +1,23 @@
 <x-filament-panels::page>
 
-    <h1 class="text-2xl font-bold">Halaman Publik</h1>
-    <p>Ini adalah halaman yang dapat diakses tanpa login.</p>
+    <!-- <h1 class="text-2xl font-bold">Halaman Publik</h1>
+    <p>Ini adalah halaman yang dapat diakses tanpa login.</p> -->
 
-    <h1 class="text-2xl font-bold">Detail Record</h1>
+    <h1 class="text-2xl font-bold">INFORMASI INVENTARIS ASSET</h1>
 
     <p><strong>ID:</strong> {{ $record->id }}</p>
-    <p><strong>Nama:</strong> {{ $record->kode_asset }}</p>
-    <p><strong>Deskripsi:</strong> {{ $record->nama_asset }}</p>
+    <p><strong>Kode Asset:</strong> {{ $record->kode_asset }}</p>
+    <p><strong>Nama:</strong> {{ $record->nama_asset }}</p>
+    <p><strong>Tanggal Pembelian:</strong> {{ $record->tgl_beli }}</p>
+    <p><strong>Harga:</strong> {{ $record->hrg_beli }}</p>
+    <p><strong>Ruang/Lokasi:</strong> {{ $record->ruangan->lokasi }}</p>
+    <p><strong>Divisi:</strong> {{ $record->ruangan->ruangan }}</p>
+    <p><strong>Status Barang:</strong> {{ $record->status_barang }}</p>
+    <p><strong>Gambar Barang:</strong> {{ $urlGambar }}
 
 
+    <img src="{{ url("storage/".$record->gambar) }}" alt="" title="" width="300" height="300"/>
+</p>
    
     <x-filament::input.wrapper>
     <x-filament::input
