43e4240805abbc0c930f6a95363609179e8ac81f
Yakin <sureonly88@gmail.com>
2025-03-02 21:56:53 +0800

    tambah

--- app/Filament/Pages/InfoAssetPublic.php
+++ app/Filament/Pages/InfoAssetPublic.php
@@ -4,6 +4,7 @@ namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
 use App\Models\Asset;
+use Illuminate\Support\Facades\Storage;
 
 class InfoAssetPublic extends Page
 {
@@ -11,15 +12,18 @@ class InfoAssetPublic extends Page
 
     protected static string $view = 'filament.pages.info-asset-public';
 
-    protected static ?string $title = 'Informasi Asset';
+    protected static ?string $title = 'KOPERASI KONSUMEN PEDAMI';
 
     protected static bool $shouldRegisterNavigation = false; 
 
     public $record;
 
+    public $urlGambar;
+
     public function mount($id)
     {
         $this->record = Asset::findOrFail($id);
+        $this->urlGambar = Storage::url(Asset::findOrFail($id)->gambar);
     }
 
 }
