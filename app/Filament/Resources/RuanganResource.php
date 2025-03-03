882020a38c7cdcd4c17d0bb7c99849f3d86d9eaa
Yakin <sureonly88@gmail.com>
2025-03-03 08:33:09 +0800

    edit ruangan

--- app/Filament/Resources/RuanganResource.php
+++ app/Filament/Resources/RuanganResource.php
@@ -40,7 +40,7 @@ class RuanganResource extends Resource
                         'Jl. Sutoyo S. (Tower PTAM Bandarmasih)' => 'Jl. Sutoyo S. (Tower PTAM Bandarmasih)',
                         'Jl. Cemara (Kantor Bantu PTAM Bandarmasih)' => 'Jl. Cemara (Kantor Bantu PTAM Bandarmasih)',
                         'Jl. A. Yani Km. 2 Kebun Bunga' => 'Jl. A. Yani Km. 2 Kebun Bunga',
-                        'Jl. Beruntung' => 'Beruntung',
+                        'Jl. Beruntung' => 'Jl. Beruntung Km. 7',
                         'Jl. Banjar Indah' => 'Jl. Banjar Indah',
                     ])
             ]);
